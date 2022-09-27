23c9a45eb36ef9b34dcd066ace50fa51d481022b
daddyrusher <daddyrusher@gmail.com>
2022-09-28 01:15:46 +0300

    Initial commit

---
+++ src/main/java/com/daddyrusher/booksservicekubernetes/BooksServiceKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.daddyrusher.booksservicekubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksServiceKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksServiceKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daddyrusher/booksservicekubernetes/BooksServiceKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daddyrusher.booksservicekubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksServiceKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
